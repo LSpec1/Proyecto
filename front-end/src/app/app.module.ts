c0948b2bef88d4c17529e64048e97c438d0a36b5
Elian <toroelian4@gmail.com>
2021-11-11 16:28:53 -0300

    navbar maqueta

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { NavbarScreenComponent } from './front-end/navbar/navbar-screen/navbar-screen.component';
 import { RegistrarseScreenComponent } from './front-end/registrarse/registrarse-screen/registrarse-screen.component';
 import { InicioScreenComponent } from './front-end/inicio/inicio-screen/inicio-screen.component';
 import { LoginScreenComponent } from './front-end/login/login-screen/login-screen.component';
@@ -20,7 +19,6 @@ import { EmpleoScreenComponent } from './front-end/empleo/empleo-screen/empleo-s
   declarations: [
     AppComponent,
     NavbarComponent,
-    NavbarScreenComponent,
     RegistrarseScreenComponent,
     InicioScreenComponent,
     LoginScreenComponent,
