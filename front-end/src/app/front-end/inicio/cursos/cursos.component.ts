8477e2674023c18387d38bfccca3edda50a9c6c9
Elian <toroelian4@gmail.com>
2021-11-12 19:19:54 -0300

    seccion ofertas laborales terminada

--- front-end/src/app/front-end/inicio/cursos/cursos.component.ts
+++ front-end/src/app/front-end/inicio/cursos/cursos.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
+  cartas: any[] = [
+    {
+      video: "https://www.youtube.com/embed/L1oMLsiMusQ",
+      titulo: "Curso de programación",
+      texto: "Módulo 2: Clase 5"
+    },{
+      video: "https://www.youtube.com/embed/Cs5ymoNkrX8",
+      titulo: "Curso de diseño",
+      texto: "Módulo 1: Clase 1"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
