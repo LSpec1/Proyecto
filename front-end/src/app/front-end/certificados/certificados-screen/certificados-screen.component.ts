497669f12b82d15efcfcab7eb1b9bc06c0deffe8
archesspect <megazetap@gmail.com>
2021-11-18 17:26:43 -0300

    Creación  ventana certificados

--- front-end/src/app/front-end/certificados/certificados-screen/certificados-screen.component.ts
+++ front-end/src/app/front-end/certificados/certificados-screen/certificados-screen.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificadosScreenComponent implements OnInit {
 
+    certificados: any[] = [
+      {
+        imagen: "certificado.png",
+        titulo: "Curso de Programación",
+        certificado: "certificadocurso.jpg",
+      },
+      {
+        imagen: "certificado.png",
+        titulo: "Curso de Diseño UI/UX",
+        certificado: "certificadocurso.jpg",
+      },
+      {
+        imagen: "certificado.png",
+        titulo: "Curso de Diseño Gráfico",
+        certificado: "certificadocurso.jpg",
+      }
+
+    ]
+
+    otroscursos: any[] = [
+      {
+        video: "https://www.youtube.com/embed/L1oMLsiMusQ",
+        titulo: "Curso de programación",
+      },{
+        video: "https://www.youtube.com/embed/Cs5ymoNkrX8",
+        titulo: "Curso de diseño",
+      },{
+        video: "https://www.youtube.com/embed/Cs5ymoNkrX8",
+        titulo: "Curso de programación web",
+      }
+
+    ]
+
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
