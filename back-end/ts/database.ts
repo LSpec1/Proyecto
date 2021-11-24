3c0f009c4ba61645dbe29f13024d4e04bb2ad2a3
archesspect <megazetap@gmail.com>
2021-11-24 20:23:01 -0300

    cambio clave postgres

--- back-end/ts/database.ts
+++ back-end/ts/database.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export const pool = new Pool ({
     user: 'postgres',
-    password: 'naruto123',
+    password: 'root',
     host: 'localhost',
     port: 5432,
     database: 'Contenidos'
