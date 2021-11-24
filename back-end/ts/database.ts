ddef9de235b59d1dc5c4e75bdcfdba73a7643607
Elian <toroelian4@gmail.com>
2021-11-24 10:11:30 -0300

    archivos faltantes

---
+++ back-end/ts/database.ts
@@ -0,0 +1,9 @@
+import { Pool } from 'pg';
+
+export const pool = new Pool ({
+    user: 'postgres',
+    password: 'naruto123',
+    host: 'localhost',
+    port: 5432,
+    database: 'Contenidos'
+});
